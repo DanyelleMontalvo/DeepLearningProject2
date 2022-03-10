0b2abd60341bb527071fe205c205c51c3ebe6313
Danyelle Loffredo <danyellemontalvo@gmail.com>
2022-03-10 14:47:30 -0700

    Updated Bayes

--- NaiveBayes.py
+++ NaiveBayes.py
@@ -20,45 +20,60 @@ def csv_to_sparse(csvdoc, colnum):
                     col_count.append(idx)
                     data_count.append(int(el))
             rowcount = rowcount + 1
-    new_matrix = csr_matrix((data_count, (row_count, col_count)), shape=(rowcount, 12))
+    new_matrix = csr_matrix((data_count, (row_count, col_count)), shape=(rowcount, 61190))
     return new_matrix, rowcount
 
 def classify(csvdoc, p_v, probs_calc):
     testid = 0
     max_prob = -100000
     max_idx = 0
+    Map_calc = 0
+    row_count = 0
+    file_object = open('sample.csv', 'w+')
+    file_object.write("id,class\n")
     with open(csvdoc, newline='', encoding='utf-8-sig') as csvfile:
         csvreader = csv.reader(csvfile, delimiter=',')
         for training_row in csvreader:
             testid = training_row[0]
-            del row[0]
+            del training_row[0]
             for idx, row in enumerate(probs_calc):
-                for idxrow, el in enumerate(row):
+                for idxrow, el in enumerate(training_row):
                     if int(el) == 0:
                         continue
                     else:
-                        Map_calc = Map_calc + probs_calc * int(el)
+                        Map_calc = Map_calc + row[idxrow] * int(el)
+                Map_calc = Map_calc + p_v[row_count]
+                print(Map_calc)
+                row_count = row_count + 1
                 if Map_calc > max_prob:
                     max_prob = Map_calc
-                    max_idx = idx
-        
+                    max_idx = idx + 1
+                Map_calc = 0
+            row_count = 0
+            file_object.write(str(testid) + ","+str(max_idx)+'\n')
+            print(max_prob)
+            print(max_idx)
+            max_prob = -10000
+            max_idx = 0
 
 
 
 
-Vocabulary = 10
+
+Vocabulary = 61188
 beta = 1/Vocabulary
 p_v = []
 prob_calcs = []
-text_matrix, total_rows = csv_to_sparse('exampletext.csv', 12)
-for classnum in range(1, 5):
-    filtered_matrix = text_matrix[text_matrix[:, 11] == classnum].tolist()[0]
+text_matrix, total_rows = csv_to_sparse('training.csv', 61190)
+for classnum in range(1, 21):
+    filtered_matrix = text_matrix[text_matrix[:, 61189] == classnum].tolist()[0]
     #code from https://stackoverflow.com/questions/4918425/subtract-a-value-from-every-number-in-a-list-in-python
     filtered_matrix_list = [x - 1 for x in filtered_matrix]
     num_rows = len(filtered_matrix_list)
     probs = math.log2(num_rows/total_rows)
     p_v.append(probs)
     man_data = text_matrix[filtered_matrix_list, :].sum(axis=0).tolist()[0]
+
     del man_data[0]
     del man_data[-1]
     total_words = sum(man_data)
@@ -66,11 +81,14 @@ for classnum in range(1, 5):
     func = lambda x: math.log2((x + beta)/(total_words + 1))
     vfunc = np.vectorize(func)
     prob_bayes = vfunc(man_data)
+    print(prob_bayes)
     prob_calcs.append(prob_bayes)
+print(p_v)
+print(prob_calcs)
 
 
 
-classify('exampletext.csv', p_v,prob_calcs)
+classify('testing 3.csv', p_v,prob_calcs)
 
 
 
