8c672c4e9691cf52ba5a86c8d5845a7c75ea545f
LoboBAT <bockj1998@unm.edu>
2022-03-14 01:38:44 -0600

    Create initial matrices

--- LogReg.py
+++ LogReg.py
@@ -8,20 +8,25 @@ from numpy import genfromtxt
 import numpy as np
 import csv
 import statistics as st
+from scipy.sparse import csr_matrix
+
 
 def to_sparse_mat(file):
     csv = np.genfromtxt (file, delimiter=",")
     numpy_array = csv[:,1:]
     #Build Y as described in Proj. 2 description
-    Y = numpy_array[:,-1]
+    Y_np = numpy_array[:,-1]
     numpy_array = numpy_array[:,:-1]
     rows, columns = numpy_array.shape
     ones = np.ones((rows,1))
     #X as described in Proj. 2 description
     final_array = np.append(ones,numpy_array,1)
     #k as described in Proj. 2 description
-    unique_classes = len(np.unique(Y))
+    unique_classes = len(np.unique(Y_np))
     #init delta as described in Proj. 2 description (k x m matrix of zeros)
     delta=np.zeros((unique_classes,columns))
-
+    #Sparse versions of matrices
+    X = csr_matrix(final_array)
+    Y = csr_matrix(Y_np)
+    delta_sparse = csr_matrix(delta)
     
