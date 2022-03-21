1b989533b47cfc64669f332cbefaaaff5ce61ced
LoboBAT <bockj1998@unm.edu>
2022-03-21 16:09:51 -0600

    LogReg in progress

--- LogReg.py
+++ LogReg.py
@@ -28,24 +28,30 @@ def to_sparse_mat(file):
     #init delta as described in Proj. 2 description (k x m matrix of zeros)
     delta=np.zeros((unique_classes,rows))
     #Sparse versions of matrices
-    # X = csr_matrix(final_array)
-    # Y = csr_matrix(Y_np.T)
+    # X_sparse = csr_matrix(final_array)
+    # Y_sparse = csr_matrix(Y_np.T)
     # delta_sparse = csr_matrix(delta)
     
     #NumPy versions of Matrices. These handled dense-sparse multiplication better. Will look into further
     X = (final_array)
     Y = (Y_np.T)
     delta_sparse = (delta)
+    #return X_sparse, Y_sparse, delta_sparse
     return X, Y, delta_sparse
     
     #Prelim Grad descent. Need to work out P(Y|W,X) and lambda
     #need to get delta update implemented (I believe using sigma func.)
+    # Having shape issues when doing dense-sparse mat mult with csr_mat. have NumPy set up though
 def grad_descent(X, Y, delta, lamb, learning_rate, iterations):
     rows, columns = X.shape
     W = np.random.rand(len(np.unique(Y)),columns)
+    #W_sparse = csr_matrix(W)
     for i in range(0,iterations):
         Ps = np.matmul(W,(X.T))
+        #Psparse = W_sparse.multiply(X.T)
         W = W + learning_rate*(np.matmul((delta-Ps,X))-lamb*W)
+        #W_sparse = W_sparse + learning_rate*(((delta-Psparse).multiply(X))-lamb*W_sparse)
+
         print("W ",i) 
         print(W)
 if __name__ == "__main__":
