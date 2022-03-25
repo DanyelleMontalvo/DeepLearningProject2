e08dd85914f3593c95b377b5b6879bea4e465f65
bliu1013 <bliu1013@unm.edu>
2022-03-25 12:51:52 -0600

    Classification done

--- LogReg.py
+++ LogReg.py
@@ -13,7 +13,13 @@ from scipy.sparse import csr_matrix
 
 def to_sparse_mat(file):
     csv = np.genfromtxt (file, delimiter=",")
+
+    # with open(file, newline='') as csvfile:
+    #     reader = csv.reader(csvfile)
+    #     for row in reader:
+
     numpy_array = csv[:,1:]
+    print(numpy_array)
     #Build Y as described in Proj. 2 description
     Y_np = numpy_array[:,-1]
     numpy_array = numpy_array[:,:-1]
@@ -51,6 +57,16 @@ def grad_descent(X, Y, delta, lamb, learning_rate, iterations):
     for i in range(0,iterations):
         Ps = np.matmul(W,(X.T))
         Ps = np.exp(Ps)
+
+        #Set bottom row to 1's and normalize each column
+        Ps[len(Ps)-1] = np.ones_like(Ps[0])
+        for col in range(len(Ps[0])):
+            sum = 0
+            for i in range(len(Ps)):
+                sum += Ps[i][col]
+            for i in range(len(Ps)):
+                Ps[i][col] /= sum
+
         #Psparse = W_sparse.multiply(X.T)
         W = W + learning_rate*(np.matmul(delta-Ps,X)-lamb*W)
         #W_sparse = W_sparse + learning_rate*(((delta-Psparse).multiply(X))-lamb*W_sparse)
@@ -60,26 +76,48 @@ def grad_descent(X, Y, delta, lamb, learning_rate, iterations):
 
     return W
 
-if __name__ == "__main__":
-     results = to_sparse_mat("test.csv")
-     W = grad_descent(results[0], results[1], results[2], .001, .001, 1000)
 
-     #Solve exp(W * Xt) and set the bottom row to 1's
-     probMatrix = np.exp(np.matmul(W, np.matrix.transpose(results[0])))
-     probMatrix[len(probMatrix)-1] = np.ones_like(probMatrix[0])
-
-     #Normalize each column
-     for col in range(len(probMatrix[0])):
-        sum = 0
-        for i in range(len(probMatrix)):
-            sum += probMatrix[i][col]
-
-        for i in range(len(probMatrix)):
-            probMatrix[i][col] /= sum
-
-    #Classify using this matrix
+def classify(file, Y, W):
+    Y = list(set(Y))
     
+    with open(file, newline='') as test_data:
+        with open('solution.csv', 'w', newline='') as solution:
+            data_reader = csv.reader(test_data)
+            solout = csv.writer(solution)
+            solout.writerow(['id', 'class'])
 
-     print(probMatrix)
+            for example in data_reader:
+                X = example[1:]
+                X = [int(x) for x in X]
 
-     
+                #List of P(Y=yk | X), max probability is the classifier
+                probs = []
+                K = len(Y) - 1
+                n = len(X) - 1
+                for k, y in enumerate(Y):
+                    if k != K:
+                        num = np.exp(W[k][0] + sum(W[k][i] * X[i] for i in range(n)))
+                        denom = 1
+                        for j in range(K-1):
+                            denom += np.exp(W[j][0] + sum(W[j][i] * X[i] for i in range(n)))
+            
+                        p = num / denom
+                        probs.append(p)
+                    else:
+                        denom = 1
+                        for j in range(K-1):
+                            denom += np.exp(W[j][0] + sum(W[j][i] * X[i] for i in range(n)))
+
+                        p = 1 / denom
+                        probs.append(p)
+
+                idx = np.argmax(probs)
+                ans = [int(example[0]), int(Y[idx])]
+
+                print(ans)
+                solout.writerow(ans)
+
+if __name__ == "__main__":
+    results = to_sparse_mat("test_train.csv")
+    W = grad_descent(results[0], results[1], results[2], .001, .001, 1000)
+    classify("test_tester.csv", results[1], W)
